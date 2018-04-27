299cc12e358d1a9a8b22ca3aca2feaf90ec2108e
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-04-27 03:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  return object
+  return object[key] = value
 }
